f5a7cb9cca06bc0c73786d53994fe65afce54a6d
Victor Garcia <vgpastor@ingenierosweb.co>
2020-03-29 20:50:28 +0200

    Set Zero in pression for sensor BME280

--- Firmware/aire_apollo/src/mksBME280.h
+++ Firmware/aire_apollo/src/mksBME280.h
@@ -4,25 +4,23 @@
 #include "ApolloPressureSensor.h"
 #include <Adafruit_BME280.h>
 
- #define ATM_PRESURE_PASCAL 95100
-
 /**
  * @brief BME280 I2C sensor
  *
  */
 class mksBME280 : public ApolloPressureSensor
 {
-    public:
-        mksBME280(uint8_t addr = 0x76);
-        ~mksBME280();
+public:
+    mksBME280(uint8_t addr = 0x76);
+    ~mksBME280();
 
-        bool begin();
-        float readPascal();
+    bool begin();
+    float readPascal();
 
-    private:
-        uint8_t         _addr;
-        Adafruit_BME280 _bme; // I2C
+private:
+    uint8_t _addr;
+    Adafruit_BME280 _bme; // I2C
+    float zero;
 };
-
 
 #endif

eb151900c4cc2549cca6d0721640906012a99aa6
SuccessByFailure <successbyfailure@gmail.com>
2020-05-10 00:06:24 +0200

    añadido fichero strings.h

---
+++ Firmware/aire_apollo/include/strings.h
@@ -0,0 +1,47 @@
+#ifndef STRINGS_H
+#define STRINGS_H
+
+#define STR_MEDICAL_DATA  "medicalData"
+#define STR_PRESSURE      "pressure"
+#define STR_INPUT_FLOW    "inputFlow"
+#define STR_OUTPUT_FLOW   "outputFlow"
+#define STR_VOLUME        "volume"
+
+#define STR_VENT_STATUS     "ventilatorStatus"
+#define STR_HARDWARE_STATUS "hardwareStatus"
+#define STR_OK              "ok"
+#define STR_UNCAL           "uncal"
+#define STR_ERROR           "error"
+#define STR_RUNNING         "running"
+#define STR_STOPPED         "stopped"
+#define STR_PAUSED          "paused"
+#define STR_ERROR_MSG       "errorMSG"
+//TODO.... error messages!
+
+
+#define STR_HARDWARE_INFO "hardwareInfo"
+#define STR_INPUT_STATUS  "iValveStatus"
+#define STR_OUTPUT_STATUS "oValveStatus"
+#define STR_INPUT_TARGET  "iValveTarget"
+#define STR_OUTPUT_TARGET "oValveTarget"
+
+#define STR_VENTILATOR_CONFIG     "ventilatorConfig"
+#define STR_RESPIRATIONS_PER_MIN  "rpm"
+#define STR_MAX_PRESSURE          "pMax"
+#define STR_PEAK_PRESSURE         "pPeak"
+#define STR_PEEP_PRESSURE         "pPeep"
+#define STR_TIDAL_VOLUME          "vTidal"
+#define STR_IE_RATIO              "ieRatio"
+#define STR_INSPIRATORY_RISE_TIME "iRiseTime"
+
+
+#define STR_COMMAND           "command"
+#define STR_COMMAND_START     "start"
+#define STR_COMMAND_STOP      "stop"
+#define STR_COMMAND_PAUSE     "pause"
+#define STR_COMMAND_TEST      "test"
+#define STR_COMMAND_CALIBRATE "calibrate"
+
+
+
+#endif

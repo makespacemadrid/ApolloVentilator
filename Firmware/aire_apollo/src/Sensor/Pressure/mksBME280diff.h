512a50e8095281872407e5d1c475ab1d19dd0e4d
Miguel Romaní <mr3188@users.noreply.github.com>
2020-05-15 15:54:04 +0200

    Added I2C multiplexer to diff sensor

--- Firmware/aire_apollo/src/Sensor/Pressure/mksBME280diff.h
+++ Firmware/aire_apollo/src/Sensor/Pressure/mksBME280diff.h
@@ -4,6 +4,8 @@
 #include "ApolloPressureSensor.h"
 #include <Adafruit_BME280.h>
 
+#define TCAADDR 0x70
+
 /**
  * @brief BME280 differential I2C sensor
  *
@@ -11,18 +13,20 @@
 class mksBME280diff : public ApolloPressureSensor
 {
 public:
-    mksBME280diff(uint8_t addr = 0x76,uint8_t addrOffset_ = 0x77);
+    mksBME280diff(uint8_t addr = 0x76,uint8_t addrOffset_ = 0x77, uint8_t i2cBus = 0);
     ~mksBME280diff();
 
     bool begin();
     float readPascal();
 
 private:
+    void i2cBusSelect(uint8_t busNumber);
     uint8_t addr;
     uint8_t addrOffset;    
     Adafruit_BME280 bme; // I2C
     Adafruit_BME280 bmeOffset; // Ambient sensor
     float zero;
+    uint8_t i2cBus;
 };
 
 #endif

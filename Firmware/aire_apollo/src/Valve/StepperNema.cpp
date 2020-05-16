c8677b241fb9993a0618d233f5b58667d6bb4b38
Miguel Romaní <mr3188@users.noreply.github.com>
2020-05-16 21:11:46 +0200

    Revert "Inicializa correctamente nextActionTime"
    This reverts commit c9988e1db73af1b46285becc860b9d22e3d7d45f.

--- Firmware/aire_apollo/src/Valve/StepperNema.cpp
+++ Firmware/aire_apollo/src/Valve/StepperNema.cpp
@@ -29,7 +29,7 @@ bool StepperNema::moveAwayMinEndStop()
 
 bool StepperNema::moveTowardsMinEndStop()
 {
-  uint32_t counter = 0;
+  int counter = 0;
   while(!isMinEndStopPressed() && counter < stepsMax)
   {
     stepMotor.move(-1);
@@ -69,7 +69,6 @@ bool StepperNema::home()
   int endStopHit = 0;
   if(isMinEndStopPressed()) // Si Ya estamos pisando el final de carrera nos retiramos un poco
   {
-   
     Serial.print("RETRACT...");
     endStopHit++;
     if(!moveAwayMinEndStop())
@@ -320,18 +319,13 @@ void StepperNema::highFreqUpdate()
     return;
   }
   else
-  {  
-    unsigned long mic = micros();
-    long t = nextActionTime - mic;
-    if(t > 5)
+  {
+    if(nextActionTime - micros() < 5)
     {
       blockUpdate = false;
       return;
     }
-    nextActionTime = stepMotor.nextAction();
-    mic = micros();
-    nextActionTime += mic;
-    
+    nextActionTime = micros()+stepMotor.nextAction();
     if(this->lastDir){
       this->lastPos++;
     }else{
@@ -339,7 +333,5 @@ void StepperNema::highFreqUpdate()
     }
   }
   blockUpdate = false;
-
-  
 //  Serial.println("stepper->"+String(this->stepDestination)+","+String(this->lastDir)+","+String(this->lastStep));
 }
